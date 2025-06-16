073e4e1581b77e56ac5db244ecc56955e533f679
Syazwan <hemedp1@hosiden.com>
2025-06-16 12:11:30 +0800

    test

--- Components/Program.cs
+++ Components/Program.cs
@@ -10,7 +10,7 @@ namespace HRAdmin
     {
         /// <summary> syazwan
         /// The main entry point for the application.
-        /// </summary> sagi syazwan 130625dd
+        /// </summary> callmewan
         [STAThread]
         static void Main()
         {
