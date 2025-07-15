4d7f2f7995f48ffa79a7c11aa6a4c48617b86f86
Sagilessvaran Vijayaraj <sagiless0204@gmail.com>
2025-07-15 10:57:35 +0800

    Update in UC_M_Work

--- UserControl/UC_M_Work&Benefit.cs
+++ UserControl/UC_M_Work&Benefit.cs
@@ -422,8 +422,8 @@ namespace HRAdmin.UserControl
         private void btnBack_Click(object sender, EventArgs e)
         {
             Form_Home.sharedLabel.Text = "Account > Miscellaneous Claim";
-            Form_Home.sharedbtnMCReport.Visible = true;
-            Form_Home.sharedbtnApproval.Visible = true;
+            //Form_Home.sharedbtnMCReport.Visible = true;
+            //Form_Home.sharedbtnApproval.Visible = true;
 
             UC_M_MiscellaneousClaim ug = new UC_M_MiscellaneousClaim(loggedInUser, loggedInDepart, loggedInIndex);
             addControls(ug);
